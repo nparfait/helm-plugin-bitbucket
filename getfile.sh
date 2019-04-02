59a58013fb54026ba8146b8513a12722525876b8
Nicolas Parfait <nparfait@gmail.com>
2019-04-02 17:11:19 +1100

    fixadd bitbucket https

--- getfile.sh
+++ getfile.sh
@@ -18,7 +18,7 @@ git init --quiet
 if [ "bitbucket" = $PROVIDER ]; then
   git remote add origin git@bitbucket.org:$REPO.git
 elif [ "bitbucket-https" = $PROVIDER ]; then
-  git remote add origin https://bitbucket.org:$REPO.git
+  git remote add origin https://bitbucket.org/$REPO.git
 else
   git remote add origin git@$PROVIDER.com:$REPO.git
 fi
